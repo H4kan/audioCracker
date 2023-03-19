9eea045b24f19ba31dd393877064b10d4d3508b8
Szymon Sieradzki <szsie@eg.dk>
2023-03-19 17:40:16 +0100

    fix for frequency analysers

--- Analysis/Frame/AmdfFreqAnalyser.cs
+++ Analysis/Frame/AmdfFreqAnalyser.cs
@@ -25,12 +25,20 @@ namespace audioCracker.Analysis
             {
                 return 0;
             }
+
+            // calculate range for which we will evaluate corr
+            var localMaxDelay = (int)Math.Floor(listedData.Count * 0.9);
+            var localMaxRange = listedData.Count - localMaxDelay;
+            if (localMaxRange > maxDelay)
+            {
+                localMaxRange = maxDelay;
+            }
             var currCorr = 0.0;
             var prevCorr = double.MaxValue;
-            int maxFreq = 50;
-            for (int freq = 50; freq < maxDelay; freq++)
+            int maxFreq = localMaxRange / 3;
+            for (int freq = localMaxRange / 3; freq < 2 * localMaxRange; freq++)
             {
-                currCorr = EvaluateFreqCorrelation(listedData, freq);
+                currCorr = EvaluateFreqCorrelation(listedData, freq, localMaxRange);
                 if (currCorr < prevCorr)
                 {
                     maxFreq = freq;
@@ -41,11 +49,11 @@ namespace audioCracker.Analysis
             return 1 / seconds;
         }
 
-        private double EvaluateFreqCorrelation(List<float> data, int freq)
+        private double EvaluateFreqCorrelation(List<float> data, int freq, int range)
         {
             double sum = 0.0;
 
-            for (int i = 0; i < data.Count - maxDelay; i++)
+            for (int i = 0; i < range; i++)
             {
                 sum += Math.Abs(data[i] - data[i + freq]);
             }
--- Analysis/Frame/CorFreqAnalyser.cs
+++ Analysis/Frame/CorFreqAnalyser.cs
@@ -26,12 +26,20 @@ namespace audioCracker.Analysis
             {
                 return 0;
             }
+
+            // calculate range for which we will evaluate corr
+            var localMaxDelay = (int)Math.Floor(listedData.Count * 0.9);
+            var localMaxRange = listedData.Count - localMaxDelay;
+            if (localMaxRange > maxDelay)
+            {
+                localMaxRange = maxDelay;
+            }
             var currCorr = 0.0;
             var prevCorr = 0.0;
-            int maxFreq = 50;
-            for (int freq = 50; freq < maxDelay; freq++)
+            int maxFreq = localMaxRange / 3;
+            for (int freq = localMaxRange / 3; freq < 2 * localMaxRange; freq++)
             {
-                currCorr = EvaluateFreqCorrelation(listedData, freq);
+                currCorr = EvaluateFreqCorrelation(listedData, freq, localMaxRange);
                 if (currCorr > prevCorr)
                 {
                     maxFreq = freq;
@@ -42,11 +50,11 @@ namespace audioCracker.Analysis
             return 1 / seconds;
         }
 
-        private double EvaluateFreqCorrelation(List<float> data, int freq)
+        private double EvaluateFreqCorrelation(List<float> data, int freq, int range)
         {
             double sum = 0.0;
 
-            for (int i = 0; i < maxDelay; i++)
+            for (int i = 0; i < range; i++)
             {
                 sum += data[i] * data[i + freq];
             }
